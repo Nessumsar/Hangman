a879a73f3b24979e2f38cca9ea815c2e97eafa77
Nessumsar <54313842+Nessumsar@users.noreply.github.com>
2019-09-11 08:25:20 +0200

    Update HangMan.java

--- src/main/java/se/ecutb/lukas/HangMan.java
+++ src/main/java/se/ecutb/lukas/HangMan.java
@@ -15,20 +15,11 @@ public class HangMan {
         setupArray();
     }
 
-    public static String getString(){
-        return sc.nextLine();
-    }
-
-    public void setupArray(){
-        for (int i=0; i<secretWord.length(); i++) {  guessArray[i] = '_'; }
-    }
+    public void setupArray(){ for (int i=0; i<secretWord.length(); i++) {  guessArray[i] = '_'; } }
 
     public char[] run(String guess) {
-
         while (!winner && guessAmount != 9) {
-            System.out.println("Make a guess. The word is " + secretWord.length() + " characters long.");
             System.out.println("Guess " + guessAmount + "/8");
-            // Present word and guessAmount
             String temp = madeGuesses.toString();
             if (temp.contains(guess)) {
                 System.out.println("You've already guessed " + guess);
@@ -36,8 +27,7 @@ public class HangMan {
                 boolean valid = false;
                 int index = 0;
                 if ((guess.length() > 1 && guess.length()<secretWord.length())){
-                    System.out.println("Letter not found. To guess a word, please guess a word of the same length as the secret word.");
-                }
+                 System.out.println("Letter not found. To guess a word, please guess a word of the same length as the secret word."); }
                 if (secretWord.contains(guess) && guess.length() == 1){
                     System.out.println("Correct! \n");
                     if(secretWord.contains(guess)){
@@ -45,39 +35,23 @@ public class HangMan {
                         char convertedGuess = guess.charAt(0);
                         if (guess.length() == 1){
                             guessArray[index] = convertedGuess;
-                            for (int i=0; i<guessArray.length; i++){
-                                System.out.print(guessArray[i]);
-                            }
+                            for (int i=0; i<guessArray.length; i++){ System.out.print(guessArray[i]);  }
                         }
                     }
                     valid = true;
                 }
-                else if (!secretWord.equalsIgnoreCase(guess)) {
-                    System.out.println("Wrong guess.");
-                }
-                if (!valid){
-                    guessAmount = guessAmount + 1;
-                }
+                else if (!secretWord.equalsIgnoreCase(guess)) { System.out.println("Wrong guess."); }
+                if (!valid){ guessAmount = guessAmount + 1; }
             }
-            // Checks if the guess is a valid guess, otherwise it tells the user so.
             if (secretWord.equalsIgnoreCase(guess)){
                 winner = true;
+                for (int i=0; i<guessArray.length; i++){ guessArray[i]= guess.charAt(i); }
             }
             String winners = new String(guessArray);
             if (winners.equalsIgnoreCase(secretWord)){ winner = true; }
-            //
-            if (winner){
-                System.out.println("\nYou win! The word was "+secretWord);
-                break;
-            } if (guessAmount == 9){
-                System.out.println("You lose.");
-                break;
-                //Checks if the user has won
-            }
-            if (guess.length() == 1 || guess.length() == secretWord.length()) {
-                madeGuesses.append(" " + guess);
-            }
-            // Adds the guess to the guesses made
+            if (winner){ System.out.println("\nYou win! The word was "+secretWord); break; }
+            if (guessAmount == 9){ System.out.println("You lose.");break; }
+            if (guess.length() == 1 || guess.length() == secretWord.length()) { madeGuesses.append(" " + guess); }
             System.out.println("\nYour guesses:" + madeGuesses);
             break;
         }
