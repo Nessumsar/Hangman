15ed71fc619eb0dc8706283006f1a15d923bc3b6
Nessumsar <54313842+Nessumsar@users.noreply.github.com>
2019-09-11 08:24:56 +0200

    Update App.java

--- src/main/java/se/ecutb/lukas/App.java
+++ src/main/java/se/ecutb/lukas/App.java
@@ -2,20 +2,13 @@ package se.lexicon.ecutb.lukas;
 import java.util.Scanner;
 public class App {
     private static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
         boolean win = false;
         String[] words = {"hacker", "formidable", "machine", "rain", "water"};
         String secretWord = words[(int) (Math.random() * words.length)];
         System.out.println("Welcome to Hangman. Make a guess consisting of a character or the whole word. You got 8 guesses.");
         HangMan game = new HangMan(secretWord);
-        do {
             System.out.println("Make a guess. The word is " + secretWord.length() + " characters long.");
-            String guess = sc.nextLine();
-           char[] temparray = game.run(guess);
-           if(temparray.equals(secretWord)){
-               win = true;
-           }
-        } while (!win);
+            String guess = sc.next();game.run(guess);
     }
 }
