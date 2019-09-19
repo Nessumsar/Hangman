1c3426263e375c22f67aee97504179dd369351e2
Lukas <54313842+Nessumsar@users.noreply.github.com>
2019-09-19 13:40:01 +0200

    Update App.java

--- src/main/java/se/ecutb/lukas/App.java
+++ src/main/java/se/ecutb/lukas/App.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class App {
     private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        boolean win = false;
         String[] words = {"hacker", "formidable", "machine", "rain", "water"};
         String secretWord = words[(int) (Math.random() * words.length)];
+        char[] temp = secretWord.toCharArray();
         System.out.println("Welcome to Hangman. Make a guess consisting of a character or the whole word. You got 8 guesses.");
         HangMan game = new HangMan(secretWord);
             System.out.println("Make a guess. The word is " + secretWord.length() + " characters long.");
-            String guess = sc.next();game.run(guess);
+            while (temp != secretWord.toCharArray()) {
+                String guess = sc.next();
+                game.run(guess);
+            }
     }
 }
