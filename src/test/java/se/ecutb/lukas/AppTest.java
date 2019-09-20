00c26471fa04575e1720782090fe90b7942c4b43
Lukas <54313842+Nessumsar@users.noreply.github.com>
2019-09-20 17:52:28 +0200

    Update AppTest.java

--- src/test/java/se/ecutb/lukas/AppTest.java
+++ src/test/java/se/ecutb/lukas/AppTest.java
@@ -1,48 +1,38 @@
 package se.lexicon.ecutb.lukas;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.Arrays;
-
 public class AppTest 
 {
-        String guess = "a";
-        String wrongGuess = "q";
-        String guessword = "hacker";
-        String wrongWord = "packer";
-        HangMan game = new HangMan(guessword);
+        private HangMan game = new HangMan("hacker");
 
         @Test
         public void RunCorrectLetter(){
-            char[] testchar = new char [5];
-           testchar = game.run(guess); //Act
+            char[] testchar;
+           testchar = game.run("a");
           String temp = Arrays.toString(testchar);
            Assert.assertEquals("[_, a, _, _, _, _]", temp);
-            //Testar om run returnerar array med a på rätt plats vilket görs
         }
     @Test
     public void RunWrongLetter(){
-        char[] testchar = new char [5];
-        testchar = game.run(wrongGuess); //Act
+        char[] testchar;
+        testchar = game.run("q");
         String temp = Arrays.toString(testchar);
         Assert.assertEquals("[_, _, _, _, _, _]", temp);
-        //Testar om run returnerar tomt array vid gissning på fel bokstav
     }
     @Test
     public void RunWrongWord(){
-        char[] testchar = new char [5];
-        testchar = game.run(wrongWord); //Act
+        char[] testchar;
+        testchar = game.run("packer");
         String temp = Arrays.toString(testchar);
         Assert.assertEquals("[_, _, _, _, _, _]", temp);
-        //Testar om run returnerar ett tomt array om man gissar fel ord
     }
     @Test
     public void RunCorrectWord(){
-        char[] testchar = new char [5];
-        testchar = game.run("hacker"); //Act
+        char[] testchar ;
+        testchar = game.run("hacker");
         String temp = Arrays.toString(testchar);
         Assert.assertEquals("[h, a, c, k, e, r]", temp);
-        //Testar om run returnerar rätt ord vid gissning på rätt ord
     }
-}
 
+}
