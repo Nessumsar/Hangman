e131c4eb2fe658c624e152dbb8d57ca3df2dc82e
Lukas <54313842+Nessumsar@users.noreply.github.com>
2019-09-20 17:51:23 +0200

    Update App.java

--- src/main/java/se/ecutb/lukas/App.java
+++ src/main/java/se/ecutb/lukas/App.java
@@ -5,13 +5,19 @@ public class App {
     public static void main(String[] args) {
         String[] words = {"hacker", "formidable", "machine", "rain", "water"};
         String secretWord = words[(int) (Math.random() * words.length)];
-        char[] temp;
         System.out.println("Welcome to Hangman. Make a guess consisting of a character or the whole word. You got 8 guesses.");
         HangMan game = new HangMan(secretWord);
-            System.out.println("Make a guess. The word is " + secretWord.length() + " characters long.");
-           do {
+            System.out.println("Make a guess. The word is " +secretWord.length()+ " characters long. Press 0 to quit at any time.");
+        while (true){
                 String guess = sc.next();
-                temp = game.run(guess);
-            }while (temp.equals(secretWord.toCharArray()));
+                if (guess.equalsIgnoreCase("0")){
+                    break;
+                }
+            char[] temp = game.run(guess);
+                String converted = temp.toString();
+                if (converted.equalsIgnoreCase(secretWord) || guess.equalsIgnoreCase(secretWord)){
+                    break;
+                }
+            }
     }
 }
